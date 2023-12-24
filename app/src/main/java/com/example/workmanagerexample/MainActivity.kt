a193a2e9569126ed424d5e7b7520b3d9d23b1acd
F Tcymbal <ftsymbal@gmail.com>
2023-12-24 23:45:23 +0100

    Periodic Worker

--- app/src/main/java/com/example/workmanagerexample/MainActivity.kt
+++ app/src/main/java/com/example/workmanagerexample/MainActivity.kt
@@ -3,24 +3,42 @@ package com.example.workmanagerexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import androidx.work.OneTimeWorkRequest
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequest
+import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
-import androidx.work.WorkRequest
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     private lateinit var startWorkButton: Button
+    private lateinit var cancelWorkButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         startWorkButton = findViewById(R.id.startWorkButton)
+        cancelWorkButton = findViewById(R.id.cancelWorkButton)
 
         startWorkButton.setOnClickListener {
-            val myWorkRequest: WorkRequest =
-                OneTimeWorkRequest.from(MyWorker::class.java)
+            val myWorkRequest: PeriodicWorkRequest =
+                PeriodicWorkRequestBuilder<MyWorker>(
+                    15, TimeUnit.MINUTES,
+                    9, TimeUnit.MINUTES
+                ).build()
             WorkManager.getInstance(this)
-                .enqueue(myWorkRequest)
+                .enqueueUniquePeriodicWork(
+                    WORK_ID,
+                    ExistingPeriodicWorkPolicy.KEEP,
+                    myWorkRequest)
         }
+
+        cancelWorkButton.setOnClickListener {
+            WorkManager.getInstance(this).
+                    cancelUniqueWork(WORK_ID)
+        }
+    }
+    companion object {
+        private const val WORK_ID = "MyUniqueWork"
     }
 }
